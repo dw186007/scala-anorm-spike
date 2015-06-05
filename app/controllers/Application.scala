7c3ac32b3bf4cf578090ba6d798c27bd9138a4ae
lucasviola <lucasmatzenbacher@gmail.com>
2015-06-05 08:06:55 -0300

    routes and dummy actions created

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,15 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+  def list(p: Int, s: Int, f: String) = TODO
+
+  def create() = TODO
+
+  def save() = TODO
+
+  def edit(id: Long) = TODO
+
+  def update(id: Long) = TODO
+
+  def delete(id: Long) = TODO
 }
