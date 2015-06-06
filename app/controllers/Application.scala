b10aa7cae1bdea2b28be97574899f56a0af0286e
lucasviola <lucasmatzenbacher@gmail.com>
2015-06-05 22:43:11 -0300

    controller actions created

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,12 +9,13 @@ import anorm._
 
 import views._
 import models._
+import views.html.defaultpages.notFound
 
 object Application extends Controller {
 
   // 'variables'
 
-  val Home = Redirect(Routes.Application.list(0, 2, ""))
+  val Home = Redirect(routes.Application.list(0, 2, ""))
 
   val favoriteForm = Form(
     mapping(
@@ -29,19 +30,39 @@ object Application extends Controller {
 
   def index = Action { Home }
 
-//
-//  def list(page: Int, orderBy: Int, filter: String) = Action { implicit request =>
-//    Ok(html.list)
-//
-//  }
 
-  def create() = TODO
+  def list(page: Int, orderBy: Int, filter: String) = Action { implicit request =>
+    Ok(html.list(
+      Favorite.list(page = page, orderBy = orderBy, filter = ("%"+filter+"%")),
+      orderBy, filter
+    ))
+  }
+
+  def create() = Action {
+    Ok(html.createForm(favoriteForm, Favorite.options))
+  }
 
   def save() = TODO
 
-  def edit(id: Long) = TODO
+  def edit(id: Long) = Action {
+    Favorite.findById(id).map { favorite =>
+      Ok(html.editForm(id, favoriteForm.fill(favorite), Favorite.options))
+    }.getOrElse(NotFound)
+  }
 
-  def update(id: Long) = TODO
+  def update(id: Long) = Action { implicit request =>
+    favoriteForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.editForm(id, formWithErrors, Favorite.options)),
+      favorite => {
+        Favorite.update(id, favorite)
+        Home.flashing("success" -> "Favorite %s has been updated".format(favorite.name))
+      }
+    )
+  }
 
-  def delete(id: Long) = TODO
+  def delete(id: Long) = Action {
+    Favorite.delete(id)
+    Home.flashing("Success" -> "favorite has been deleted!")
+  }
+
 }
